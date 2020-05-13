4f47c08fd49994d9f1b7bc9d6f4139f79835b80a
virgimicci <62661686+virgimicci@users.noreply.github.com>
2020-05-13 17:31:26 +0200

    Create R_code_radiance.r

---
+++ R_code_radiance.r
@@ -0,0 +1,46 @@
+R_code_radiance.r
+
+library(raster)
+
+toy <- raster(ncol=2, nrow=2, xmn=1, xmx=2, ymn=1, ymx=2)
+values(toy) <- c(1.13,1.44,1.55,3.4)
+plot(toy)
+text(toy, digits=2)
+
+toy2bits <- stretch(toy,minv=0,maxv=3)
+storage.mode(toy2bits[]) = "integer"
+plot(toy2bits)
+text(toy2bits, digits=2)
+
+toy4bits <- stretch(toy,minv=0,maxv=15)
+storage.mode(toy4bits[]) = "integer"
+plot(toy4bits)
+text(toy4bits, digits=2)
+
+toy8bits <- stretch(toy,minv=0,maxv=255)
+storage.mode(toy8bits[]) = "integer"
+
+plot(toy8bits)
+text(toy8bits, digits=2)
+
+# plot all togheter
+par(mfrow=c(1,4))
+plot(toy)
+text(toy, digits=2)
+
+plot(toy2bits)
+text(toy2bits, digits=2)
+
+plot(toy4bits)
+text(toy4bits, digits=2)
+
+plot(toy8bits)
+text(toy8bits, digits=2)
+
+dev.off()
+library(rasterdiv)
+plot(copNDVI)
+
+
+
+
