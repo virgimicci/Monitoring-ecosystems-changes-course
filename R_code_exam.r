34bd65a329464bb484a5c2adf1b88b09b468b55c
Virginia Micci <62661686+virgimicci@users.noreply.github.com>
2020-07-12 14:44:07 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -2,15 +2,15 @@
 
 
 # Indices
-# 1. R_code_first (24)
-# 2. R_code_multipanel (52)
-# 3. R_code_spatial (92)
-# 4. R_code_multivariate (174)
-# 5. R_code_remote_sensing (209)
-# 6. densitymap_point_pattern_analysis 
-# 7.  R_code_ecosystem_functioning
-# 8.  R_code_PCA_remote_sensing 
-# 9.  R_code_faPAR  
+# 1. R_code_first 
+# 2. R_code_multipanel 
+# 3. R_code_spatial 
+# 4. R_code_multivariate 
+# 5. R_code_remote_sensing 
+# 6. R_code_point_pattern_analysis
+# 7. R_code_ecosystem_functioning
+# 8. R_code_PCA_remote_sensing 
+# 9. R_code_faPAR  
 # 10. R_code_radiance 
 # 11. R_code_faPAR10
 # 12. R_code_EBVs 
@@ -21,7 +21,9 @@
 # 17. R_code_temp_interpolation
 # 18. R_species_distribution_modelling 
 
-#### 1. R_code_first ####
+####### 1. R_code_first #######
+###############################
+
 # the function install.packages is used to install any packages that we want to use which is still not intalled in our R
 #sp is a package for spatial data
 install.packages("sp")
@@ -89,7 +91,8 @@ pairs(meuse[,3:6],pch=8,col="purple",cex=2)
 library(GGally)
 ggpairs(meuse[,3:6])
 
-#### 3. R_code_spatial  ####
+####### 3. R_code_spatial  #######
+##################################
 
 # R code for spatial view of points
 
@@ -207,6 +210,8 @@ ordispider(multivar, type, col=1:4, label=T)
 
 
 ####### 5. R_code_remote_sensing #######
+########################################
+
 # R code remote sensing, to see things through wave
 
 setwd("C:/lab/")
@@ -217,79 +222,195 @@ install.packages("RStoolbox")
 
 library(raster) 
 
+# we are going to import some images, since these are satellite images and so they have more layers we use brick
+# brick imports different layers at a time like satellites with more bands
 # .grd are the images
-p224r63_2011 <- brick("p224r63_2011_masked.grd)
-cl <- colorRampPalette(c('black','grey','light grey'))(100)
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+plot(p224r63_2011) # we can see the plot of the different reflectance of bands, now we change the color ramp palette
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+
 # Exercise: plot the image with the new color ramp palette
-plot(p224r63_2011, col=cl)
-#bands of Landsat
-#B1: blue
-#B2: green
-#B3: red
-#B4: NIR
+plot(p224r63_2011, col=cl) # using col=cl we use the colors decided in the previous funcion
+
+# Bands of Landsat
+# B1: blue
+# B2: green
+# B3: red
+# B4: NIR
+
 # colors()--> shows how many color there are into R
 # multiframe of different plots all togheter, (2,2) row per column
 par(mfrow=c(2,2))
+
 # B1: blue
 clb <- colorRampPalette(c('dark blue', 'blue', 'light blue'))(100)
-# $ to link every single ban to the image
+# $ to link every single band to the image
 plot(p224r63_2011$B1_sre, col=clb)
+
 # B2: green
-# Excercise do the ame for the green band B2_sre
+# Excercise do the same for the green band B2_sre
 clg <- colorRampPalette(c('dark green','green','light green'))(100) 
 plot(p224r63_2011$B2_sre, col=clg)
+
+# B3 red
+clr <- colorRampPalette(c('dark red','red','pink'))(100)
+plot(p224r63_2011$B3_sre, col=clr)
+
+# B4 NIR
+cln <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_2011$B4_sre, col=cln)
+
+# let's change the par
+par(mfrow=c(4,1)) # to have them all in a column
+# recall all the bands
+
 # close all the things before
 dev.off()
-# RGB
-plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
-plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
-# Excercise NIR ontop of the G component of the RGB
+
+## RGB
+# we are goin to mount the bands 
+# plotRGB, with linear stretching (to make a continuous between the colours) and associating the bands with the respective colors
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin") # how we would see it with our eyes
+
+# now we are making all the components shift and add first the NIR, removing the blue 
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin") # like this we mounted the NIR on top of the red component of the RGB spectrum so all the areas reflecting in the NIR will apear red (mainly vegetation)
+
+# put the NIR on top of the green component of the RGB
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
+
+# now NIR in the blue component
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
-#######
+
+######### Second lesson
+
 setwd("C:/lab/")
 load("RS.RData")
-ls()
+ls() #ls() is the list function to see which packages we have already
 library(raster)
 p224r63_1988 <- brick("p224r63_1988_masked.grd")
-#bands of Landsat
+plot(p224r63_1988)
+
+#Bands of Landsat
 #B1: blue
 #B2: green
 #B3: red
 #B4: NIR
+
 # Excercise: plot in visible RGB 321 both images
 par(mfrow=c(2,1)) # you are saying that tou want the two follows images toghether in one column and two rows
 plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
 plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+# we can see the two images together so we can appreciate the changes, however the vegatation is difficult to see
+# show the same plot but with 432 RGBspace, to see the NIR
+
 # Excercise: plot in false colour RGB 432 both images
 par(mfrow=c(2,1)) 
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
-# enhance the noise!
+
+# enhance the noise, stretching more the colours
 par(mfrow=c(2,1))
 plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+
 # plot RGB
 #bands of Landsat
 #B1: blue
 #B2: green
 #B3: red
 #B4: NIR
+
+# we are going to use the vegetation index
 dvi2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre
 cl <- colorRampPalette(c('darkorchid3','light blue','lightpink4'))(100) 
 plot(dvi2011,col=cl)
+
 # Excercise: dvi for 1988
 dvi1988 <- p224r63_1988$B4_sre - p224r63_1988$B3_sre
 cl <- colorRampPalette(c('darkorchid3','light blue','lightpink4'))(100) 
 plot(dvi2011,col=cl)
+
 # dff from one year to the other
 diff <- dvi2011 - dvi1988
 plot(diff)
-# changing the grain
-#resampling
+
+# let's see the effect of changing the grain (dimension of the pixels) 
+# aggregate function: aggregates pixels to make a coarser grain, "fact" is the argument that indicates the factor of increase of the pixels.
+# this process is called resampling 
 p224r63_2011res <- aggregate(p224r63_2011, fact=10)
 p224r63_2011res100 <- aggregate(p224r63_2011, fact=100)
+
 par(mfrow=c(3,1))
 plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch="Lin")
 plotRGB(p224r63_2011res100, r=4, g=3, b=2, stretch="Lin")
+
+####### 6. R_code_point_pattern_analysis #######
+################################################
+
+# Point pattern analysis: density map
+
+install.packages("spatstat") # spatstat is a package for the statistical analysis of spatial point patterns
+library(spatstat)
+
+attach(covid)
+head(covid)
+# let's give a name to what we are about to make -> covids. ppp is planar point pattern 
+# then explain x and y variables and the range for the numbers with c
+covids <- ppp(lon, lat, c(-180,180), c(-90,90)) # ?ppp creates an object of class "ppp" representing a point pattern dataset in the two-dimensional plane
+# density of the covids object that we created before
+d <- density(covids)
+# to show the map i use plot
+plot(d)
+# let's put the points inside this plot
+points(covids)
+
+### Second lesson
+setwd("C:/lab/")
+load("pointpattern.RData") # I load the work previously done and saved
+library("spatstat")
+
+# density map
+plot(d)
+points(covids)
+# download coastlines .zip file from IOL and copy all the files into lab folder
+# rgdal package provides bindings to the 'Geospatial' Data Abstraction Library 
+install.packages("rgdal")
+library(rgdal)
+
+# let's input vector line (x0y0, x1y1, x2y2..)
+# #read0GR within rgdal is a function that reads an OGR data source and layer into a suitable Spatial vector object
+coastlines <- readOGR("ne_10m_coastline.shp")
+
+#add--> add to the previous plot,  it doesn't erease the previous data 
+plot(coastlines, add=T)
+
+# cl <- colorRamPalette(c('yellow', 'orange', 'red'))(100), we want red the most dense part and yellow the low dense part, 100=concern all possible color from yellow to red
+cl <- colorRampPalette(c('yellow', 'orange', 'red')) (100)
+plot(d, col=cl)
+points(covids)
+plot(coastlines, add=T)
+
+# Exercise: new colour ramp palette
+cl <- colorRampPalette(c("lightblue", "blue", "yellow", "orange", "red")) (100)
+plot(d, col=cl, main="Covid19 Density")
+points(covids)
+plot(coastlines, add=T)
+
+# In order to export the map begin with pdf(), after put all the commands for the map and finish with dev.off
+pdf("covid_density.pdf")
+cl <- colorRampPalette(c("lightblue", "blue", "yellow", "orange", "red")) (100)
+plot(d, col=cl, main="Covid19 Density")
+points(covids)
+plot(coastlines, add=T)
+dev.off()
+
+####### 7. R_code_ecosystem_functioning #######
+###############################################
+
+
+
+
+
+
+
